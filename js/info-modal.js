a148412b99e75bd86a6bd21219a01b537383cd2a
Bryan Ridpath <bryanridpath@gmail.com>
2023-04-04 15:36:17 +0100

    update 04.04.2023

--- js/info-modal.js
+++
@@ -1,6 +0,0 @@
-const modal = document.getElementById('id01');
-window.onclick = function(close) {
-    if (close.target == modal) {
-        modal.style.display = "none";
-    }
-}
--- js/window-load.js
+++
@@ -1,4 +0,0 @@
-$(window).on('load',function(){
-    $(".loader").fadeOut(1500);
-    $(".content").fadeIn(1500);
-});
