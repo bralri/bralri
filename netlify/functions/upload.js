cfc371f0c2dd2b64171a321152b38b7edb42f1fd
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-17 16:41:18 +0100

    Update upload.js

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event) => {
+    console.log(event);
     const fileBuffer = Buffer.from(event.body, 'base64');
+    console.log(fileBuffer);
     const fileName = event.queryStringParameters.fileName;
+    console.log(fileName, fileBuffer);
     const uploadsPath = path.join(process.cwd(), 'submissions', fileName);
+    console.log(uploadsPath, fileName, fileBuffer);
 
     try {
         fs.writeFileSync(uploadsPath, fileBuffer);
