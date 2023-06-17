eed11195f43f5dad590dcf4e8f4250623f0cc785
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-17 22:58:43 +0100

    Update submission.js

--- netlify/functions/submission.js
+++ netlify/functions/submission.js
@@ -1,28 +1,19 @@
-const { Storage } = require('@google-cloud/storage');
+const {Storage} = require('@google-cloud/storage');
 
 const key = JSON.parse(process.env.STORAGE_KEY_JSON);
 
 exports.handler = async (event) => {
-    const storage = new Storage({ credentials: key });
+    const storage = new Storage({credentials: key});
 
     try {
-        const fileData = Buffer.from(event.body, 'binary');
+        // const fileData = Buffer.from(event.body, 'binary');
         const fileName = event.headers['content-disposition'].split('filename=')[1].replace(/"/g, '');
-        await storage.bucket('build-a-vessel-submissions').file(fileName).save(fileData);
-
-        const headers = {
-            'Content-Type': 'application/octet-stream',
-            'Content-Disposition': `attachment; filename="${fileName}"`,
-        };
-        
-        const [file] = await storage.bucket('build-a-vessel-submissions').file(fileName).download();
-        const fileContent = file.toString('base64');
+        await storage.bucket('build-a-vessel-submissions').file(fileName).save(event.body);
 
         return {
             statusCode: 200,
-            headers: headers,
-            body: fileContent,
-            isBase64Encoded: true,
+            headers: event.headers,
+            body: event.body
         };
     } catch (error) {
         console.error('Error uploading file:', error);
