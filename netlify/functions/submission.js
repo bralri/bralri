0186516416559d6cce873c230ce37c2cbf9f6d2b
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-17 22:35:52 +0100

    Update submission.js

--- netlify/functions/submission.js
+++ netlify/functions/submission.js
@@ -3,18 +3,12 @@ const {Storage} = require('@google-cloud/storage');
 const key = JSON.parse(process.env.STORAGE_KEY_JSON);
 
 exports.handler = async (event) => {
-    // Create a new Google Cloud Storage client
     const storage = new Storage({credentials: key});
 
     try {
-        // Get the file data from the request body
         const fileData = Buffer.from(event.body, 'binary');
-
-        // Set the name for the file in the storage bucket
         const fileName = event.headers['content-disposition'].split('filename=')[1].replace(/"/g, '');
-
-        // Upload the file to the storage bucket
-        await storage.bucket('build-a-vessel-submission').file(fileName).save(fileData);
+        await storage.bucket('build-a-vessel-submissions').file(fileName).save(fileData);
 
         return {
             statusCode: 200,
