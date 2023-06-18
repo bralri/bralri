d19397506759aa10126841b6d69b60bf68fbdbea
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-18 17:42:23 +0100

    Update fetchSubmissions.js

--- netlify/functions/fetchSubmissions.js
+++ netlify/functions/fetchSubmissions.js
@@ -16,12 +16,7 @@ exports.handler = async () => {
             const month = String(date.getMonth() + 1).padStart(2, "0");
             const day = String(date.getDate()).padStart(2, "0");
 
-            let createdBy;
-            if (file.metadata.metadata.author) {
-                createdBy = file.metadata.metadata.author
-            } else {
-                createdBy = undefined;
-            }
+            let createdBy = "bryan ridpath"
 
             return {
                 name: file.name,
