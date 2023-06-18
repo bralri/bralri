6882b1d5f5ddbc6c1c12052e4c0b71db13a7ca54
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-18 17:34:11 +0100

    Update fetchSubmissions.js

--- netlify/functions/fetchSubmissions.js
+++ netlify/functions/fetchSubmissions.js
@@ -13,8 +13,8 @@ exports.handler = async () => {
             const dateString = file.metadata.timeCreated;
             const date = new Date(dateString);
             const year = date.getFullYear();
-            const month = date.getMonth();
-            const day = date.getDate();
+            const month = String(date.getMonth() + 1).padStart(2, "0");
+            const day = String(date.getDate()).padStart(2, "0");
 
             return {
                 name: file.name,
