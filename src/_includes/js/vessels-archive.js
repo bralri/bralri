e1dc075c0622acf3d935adebf5e3c4d916b17c95
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-18 16:40:47 +0100

    Update vessels-archive.js

--- src/_includes/js/vessels-archive.js
+++ src/_includes/js/vessels-archive.js
@@ -103,25 +103,25 @@ const loadAssets = () => {
         const offset = (gridSize - 1) * spacing * 0.5;
 
         fileArray.forEach((file, i) => {
-            console.log(file.file);
-            const assetInstance = createAssetInstance(file.name, file.publicUrl);
-            assetInstance.then((instance) => {
-                const row = Math.floor(i / gridSize);
-                const col = i % gridSize;
-                const x = (col * spacing) - offset;
-                const z = (row * spacing) - offset;
+            console.log(file);
+            // const assetInstance = createAssetInstance(file.name, file.publicUrl);
+            // assetInstance.then((instance) => {
+            //     const row = Math.floor(i / gridSize);
+            //     const col = i % gridSize;
+            //     const x = (col * spacing) - offset;
+            //     const z = (row * spacing) - offset;
         
-                instance.mesh.position.set(x, 50, z);
-                instance.mesh.scale.set(40, 40, 40);
-                instance.mesh.rotateY(Math.PI / -1.5);
+            //     instance.mesh.position.set(x, 50, z);
+            //     instance.mesh.scale.set(40, 40, 40);
+            //     instance.mesh.rotateY(Math.PI / -1.5);
         
-                scene.add(instance.mesh);
+            //     scene.add(instance.mesh);
         
-                objects.push(instance.mesh);
-                objectsId.push(instance.mesh.userData.id);
-            }).catch((error) => {
-                console.log(error);
-            })
+            //     objects.push(instance.mesh);
+            //     objectsId.push(instance.mesh.userData.id);
+            // }).catch((error) => {
+            //     console.log(error);
+            // })
         })
     })
 }
