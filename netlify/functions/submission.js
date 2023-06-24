4357fce539eb9aa7d80371514d14ad1ebffd12aa
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-24 12:16:09 +0100

    Update submission.js

--- netlify/functions/submission.js
+++ netlify/functions/submission.js
@@ -1,38 +1,48 @@
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 
 const key = JSON.parse(process.env.STORAGE_KEY_JSON);
-const storage = new Storage({credentials: key});
+const storage = new Storage({ credentials: key });
 const bucket = storage.bucket('build-a-vessel-submissions');
 
 exports.handler = async (event) => {
-    try {
-        const file = event.body.file;
-        const fileName = event.body.fileName;
+  try {
+    const file = event.body.file; // Assuming the file data is sent as 'file' in the request body
+    const fileName = event.body.fileName; // Assuming the file name is sent as 'fileName' in the request body
 
-        const blob = bucket.file(fileName);
-        const blobStream = blob.createWriteStream();
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream();
 
-        file.on('data', (chunk) => 
-            {
-                blobStream.write(chunk)
-            }
-        );
-        file.on('end', () => 
-            {
-                blobStream.end();
-                console.log('File uploaded successfully!');
-            }
-        );
+    blobStream.on('error', (error) => {
+      console.error('Error uploading the file:', error);
+      return {
+        statusCode: 500,
+        body: 'File upload failed.',
+      };
+    });
 
-        return {
-            statusCode: 200,
-            body: 'File upload in progress.',
-        }
-    } catch (error) {
-        console.log('An error occurred while uploading the file:', error);
-        return {
-            statusCode: 500,
-            body: 'File upload failed.',
-        }
-    }
-}
+    blobStream.on('finish', () => {
+      console.log('File uploaded successfully!');
+      return {
+        statusCode: 200,
+        body: 'File uploaded successfully!',
+      };
+    });
+
+    // Pipe the file data to the blobStream
+    file.on('data', (chunk) => blobStream.write(chunk));
+    file.on('end', () => {
+      blobStream.end();
+    });
+
+    return {
+      statusCode: 200,
+      body: 'File upload in progress.',
+    };
+  } catch (error) {
+    console.error('An error occurred while processing the request:', error);
+    return {
+      statusCode: 500,
+      body: 'File upload failed.',
+    };
+  }
+};
