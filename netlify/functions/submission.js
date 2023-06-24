f5182d5b2d27e22f6ec2c8dc9221a6d7b0589177
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-24 15:00:53 +0100

    Update submission.js

--- netlify/functions/submission.js
+++ netlify/functions/submission.js
@@ -1,63 +1,59 @@
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 const multer = require('multer');
-const upload = multer();
+const { Readable } = require('stream');
 
 const key = JSON.parse(process.env.STORAGE_KEY_JSON);
-const storage = new Storage({credentials: key});
+const storage = new Storage({ credentials: key });
 const bucket = storage.bucket('build-a-vessel-submissions');
 
+const upload = multer({ storage });
+
 exports.handler = async (event) => {
-    try {
-        const formData = upload.single('file')(event);
-        const file = formData.file;
-        const fileName = formData.fileName;
-        const userName = formData.userName;
-        console.log('fileName:', fileName);
-        console.log('userName:', userName);
+  try {
+    const formData = await new Promise((resolve, reject) => {
+      upload.single('file')(event, {}, (error) => {
+        if (error) reject(error);
+        const { file, body } = event;
+        resolve({ file, body });
+      });
+    });
 
-        const blob = bucket.file(fileName);
-        const blobStream = blob.createWriteStream();
+    const file = formData.file;
+    const fileName = formData.fileName;
+    const userName = formData.userName;
+    console.log('fileName:', fileName);
+    console.log('userName:', userName);
 
-        blobStream.on('error', (error) => 
-            {
-                console.error('Error uploading the file:', error);
-                return {
-                    statusCode: 500,
-                    body: 'File upload failed.',
-                };
-            }
-        );
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream();
 
-        blobStream.on('finish', () => 
-            {
-                console.log('File uploaded successfully!');
-                return {
-                    statusCode: 200,
-                    body: 'File uploaded successfully!',
-                };
-            }
-        );
+    blobStream.on('error', (error) => {
+      console.error('Error uploading the file:', error);
+      return {
+        statusCode: 500,
+        body: 'File upload failed.',
+      };
+    });
 
-        file.on('data', (chunk) => 
-            {
-                blobStream.write(chunk);
-            }
-        );
-        file.on('end', () => 
-            {
-                blobStream.end();
-            }
-        );
+    blobStream.on('finish', () => {
+      console.log('File uploaded successfully!');
+      return {
+        statusCode: 200,
+        body: 'File uploaded successfully!',
+      };
+    });
 
-        return {
-            statusCode: 200,
-            body: 'File upload in progress.',
-        }
-    } catch (error) {
-        console.error('An error occurred while processing the request:', error);
-        return {
-            statusCode: 500,
-            body: 'File upload failed.',
-        };
-    }
-}
+    file.stream.pipe(blobStream);
+
+    return {
+      statusCode: 200,
+      body: 'File upload in progress.',
+    };
+  } catch (error) {
+    console.error('An error occurred while processing the request:', error);
+    return {
+      statusCode: 500,
+      body: 'File upload failed.',
+    };
+  }
+};
