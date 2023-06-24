daded424f76c7e4beb683311342f329ca99838a7
Bryan Ridpath <bryanridpath@gmail.com>
2023-06-24 13:59:55 +0100

    Update vessels-archive.js

--- src/_includes/js/vessels-archive.js
+++ src/_includes/js/vessels-archive.js
@@ -102,32 +102,34 @@ const loadAssets = () => {
         const spacing = 600;
         const offset = (gridSize - 1) * spacing * 0.5;
 
-        if (!asset.url.lastIndexOf('.')+1 === 'glb') return;
-        
         array.forEach((asset, i) => {
-            const assetInstance = createAssetInstance(
-                asset.name, 
-                asset.createdBy,
-                asset.dateCreated,
-                asset.url,
-            );
-            assetInstance.then((instance) => {
-                const row = Math.floor(i / gridSize);
-                const col = i % gridSize;
-                const x = (col * spacing) - offset;
-                const z = (row * spacing) - offset;
-        
-                instance.mesh.position.set(x, 40, z);
-                instance.mesh.scale.set(30, 30, 30);
-                instance.mesh.rotateY(Math.PI / 2);
-        
-                scene.add(instance.mesh);
-        
-                objects.push(instance.mesh);
-                objectsId.push(instance.mesh.userData.id);
-            }).catch((error) => {
-                console.log(error);
-            });
+            if (asset.url.lastIndexOf('.')+1 === 'glb') {
+                const assetInstance = createAssetInstance(
+                    asset.name, 
+                    asset.createdBy,
+                    asset.dateCreated,
+                    asset.url,
+                );
+                assetInstance.then((instance) => {
+                    const row = Math.floor(i / gridSize);
+                    const col = i % gridSize;
+                    const x = (col * spacing) - offset;
+                    const z = (row * spacing) - offset;
+            
+                    instance.mesh.position.set(x, 40, z);
+                    instance.mesh.scale.set(30, 30, 30);
+                    instance.mesh.rotateY(Math.PI / 2);
+            
+                    scene.add(instance.mesh);
+            
+                    objects.push(instance.mesh);
+                    objectsId.push(instance.mesh.userData.id);
+                }).catch((error) => {
+                    console.log(error);
+                });
+            } else {
+                return;
+            }
         })
     })
 }
